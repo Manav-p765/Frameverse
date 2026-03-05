8244c16a39104eaa32ebe3d7cfac240c89590b2c
manav <manavparihar2000@gmail.com>
2026-03-05 20:01:53 +0530

    fixes

--- Frontend/frameverse/src/main.jsx
+++ Frontend/frameverse/src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Analytics />
+      <SpeedInsights />
       <App />
     </BrowserRouter>
   </StrictMode>
